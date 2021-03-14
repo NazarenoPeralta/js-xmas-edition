63077a66a03e333f458d26ab971e220a87db978f
Naza Peralta <nazaperalta@outlook.com>
2021-03-14 19:44:34 -0300

    Clase 7 y principios clase 8

--- js/main.js
+++ js/main.js
@@ -1,31 +1,61 @@
-function probarValidarNombre(nombre){
+function validarNombre(nombre){
     if (nombre.length === 0){
-        return "El campo nombre debe tener por lo menos un caracter";
+        return "Este campo debe tener al menos 1 caracter"
     }
-
-    if (nombre.length >= 50){
-        return "El campo nombre debe tener menos de 50 caracteres"
+    else if(nombre.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres"
     }
-
-    if(/^[A-z]+$/.test(nombre)){
-
-    }
-    return "";
+    if(!/^[A-z]+$/.test(nombre)){
+        return "El campo nombre solo acepta letras"
+    }else
+        return "";
 }
-
 function validarCiudad(ciudad){
     if (ciudad.length === 0){
-        return "El campo ciudad no puede estar vacio";
+        return "Este campo debe tener al menos 1 caracter";
     }
-    return"";
+    else if(ciudad.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres";
+    }else
+        return "";
 }
 
 function validarDescripcionRegalo(descripcionRegalo){
     if (descripcionRegalo.length >=100) {
-        return "El campo descripcion es muy largo";
+        return "Este campo debe tener menos de 100 caracteres";
     }else if (descripcionRegalo.length === 0){
-        return "El campo descripcion no puede estar vacio";
-    }else{
+        return "Este campo debe tener al menos 1 caracter";
+    }else if(!/[a-z0-9]+$/i.test(descripcionRegalo)){
+        return "El campo descripcion solo puede tener numeros y letras"
+    }
+    //Se agraga la "i" para que no importen las mayusculas, antes no se usó por "[A-z]"
+    //Para agregar . , y espacios else if(!/[a-z0-9,\. ]+$/.test(descripcionRegalo))
+    else
         return "";
+}
+ function validarFormulario(event){
+    const $form = document.querySelector("#carta-a-santa");
+    const errorNombre = validarNombre($form.nombre.value);
+    const nombre = $form.nombre.value;
+    //Es lo mismo que poner: nombre = document.querySelector("#nombre").nodeValue;
+    //Siempre es con el "name", por el simbolo de resta no se utiliza en "descripcion regalo"
+    const ciudad = $form.ciudad.value;
+    const comportamiento = $form.comportamiento.value;
+    const descripcionRegalo = $form.querySelector("#descipcion-regalo").value;     
+    
+    manejarErrores([errorNombre]);
+    
+    event.preventDefault();
+}
+function manejarErrores(errores){
+    errorNombre = errores[0]; //nombre
+
+    if(errorNombre){
+        $form.nombre.className = "error";
+    }else{
+        $form.nombre.className = "";
     }
 }
+
+
+validarFormulario();
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,34 +1,68 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    console.assert(
+      validarNombre('') === 
+      'Este campo debe tener al menos 1 caracter',
+      'validarNombre no validó que el nombre no sea vacío',
   );
-
-  console.assert(
+    console.assert(
       validarNombre(
           '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
       'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+      'validarNombre no validó que el nombre sea menor a 50 caracteres',
   );
-  console.assert(
+    console.assert(
       validarNombre("Fabricio") === "",
       "validarNombre fallo con un nombre valido"
   );
+    console.assert(
+        validarNombre("123123") === "El campo nombre solo acepta letras",
+        "validarNombre no validó que el nombre solo tenga letras"
+    )
 }
 
 
-
-
-function validarCiudad(ciudad){
-    if(ciudad.length === 0){
-        return "el campo no debe estar vacio"
-    }
+function probarValidarCiudad(){
     console.assert(
-        validarCiudad("Buenos Aires") === "",
-        "Validar ciudad no funciono con una ciudad valida"
+        validarCiudad("") ===
+        'Este campo debe tener al menos 1 caracter',
+        'validarCiudad no validó que la ciudad no sea vacío',
+    );
+    console.assert(
+        validarCiudad(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+        'Este campo debe tener menos de 50 caracteres',
+        'validarCiudad no validó que la ciudad sea menor a 50 caracteres',
     )
     console.assert(
-        validarCiudad("") === 0
+        validarCiudad("Buenos Aires") === "",
+        "validarCiudad no funciono con una ciudad valida"
     );
+    
 }
 
+function probarValidarDescripcionRegalo(){
+    console.assert(
+        validarDescripcionRegalo("") ===
+        'Este campo debe tener al menos 1 caracter',
+        'validarDescripcionRegalo no validó que la ciudad no sea vacío',
+    );
+    console.assert(
+        validarDescripcionRegalo(".,.,.,.,") === 
+        "El campo descripcion solo puede tener numeros y letras",
+        "La funcion validarDescipcionRegalo no valido que sólo tenga numeros y letras")
+    console.assert(
+        validarDescripcionRegalo(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+            'Este campo debe tener menos de 100 caracteres',
+            'validarDescripcionRegalo no validó que la ciudad sea menor a 100 caracteres',
+        )
+    console.assert(
+        validarDescripcionRegalo("Buenos Aires") === "",
+        "validarDescripcionRegalo no funciono con una ciudad valida"
+    );
+    
+}
+probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
+ //NOS QUEDAMOS EN EL MINUTO 50:14 CLASE 8
